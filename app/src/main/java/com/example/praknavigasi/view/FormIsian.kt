2362a13120ce34bc4f784b3c79c81d12fbc15386
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-11-07 16:14:56 +0700

    Tambahkan pilihan jenis kelamin (RadioButton)

--- app/src/main/java/com/example/praknavigasi/view/FormIsian.kt
+++ app/src/main/java/com/example/praknavigasi/view/FormIsian.kt
@@ -1,10 +1,13 @@
 package com.example.praknavigasi.view
 
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormIsian(onSubmitClick: () -> Unit) {
     var nama by remember { mutableStateOf("") }
     var alamat by remember { mutableStateOf("") }
+    var selectedGender by remember { mutableStateOf("") }
+    val jenisK = listOf("Laki-laki", "Perempuan")
 
     Scaffold(
         topBar = { TopAppBar(title = { Text("Form Isian") }) }
@@ -22,6 +25,24 @@ fun FormIsian(onSubmitClick: () -> Unit) {
                 singleLine = true,
                 modifier = Modifier.fillMaxWidth()
             )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text("Jenis Kelamin:")
+            Row(modifier = Modifier.padding(top = 8.dp)) {
+                jenisK.forEach { item ->
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.padding(end = 16.dp)
+                    ) {
+                        RadioButton(
+                            selected = selectedGender == item,
+                            onClick = { selectedGender = item }
+                        )
+                        Text(text = item)
+                    }
+                }
+            }
 
             Spacer(modifier = Modifier.height(16.dp))
 
