cceca99c6a020eb3ba614dce025e1246cdb28d29
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-11-07 16:28:55 +0700

    Tambah struktur dasar Navigasi

--- app/src/main/java/com/example/praknavigasi/navigation/Navigasi.kt
+++ app/src/main/java/com/example/praknavigasi/navigation/Navigasi.kt
@@ -1,2 +1,10 @@
 package com.example.praknavigasi.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun Navigasi(navController: NavHostController = rememberNavController()) {
+    // Navigasi awal akan diatur kemudian
+}
