59239b32bf48aecdc60f8aee654caaf0780cd107
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-11-07 16:14:18 +0700

    ambahkan input field nama dan alamat

--- app/src/main/java/com/example/praknavigasi/view/FormIsian.kt
+++ app/src/main/java/com/example/praknavigasi/view/FormIsian.kt
@@ -3,21 +3,35 @@ package com.example.praknavigasi.view
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormIsian(onSubmitClick: () -> Unit) {
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+
     Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text("Form Isian") }
-            )
-        }
+        topBar = { TopAppBar(title = { Text("Form Isian") }) }
     ) { innerPadding ->
         Column(
             modifier = Modifier
                 .padding(innerPadding)
-                .fillMaxSize()
                 .padding(24.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text("Isi data anda di bawah ini.")
+            OutlinedTextField(
+                value = nama,
+                onValueChange = { nama = it },
+                label = { Text("Nama Lengkap") },
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = alamat,
+                onValueChange = { alamat = it },
+                label = { Text("Alamat") },
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth()
+            )
         }
     }
 }
