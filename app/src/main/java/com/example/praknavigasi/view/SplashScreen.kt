05758d933ba554cf14ef1ccb8810f5eb334df39b
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-11-07 16:00:09 +0700

    memperbaiki import yang ada di slashscreen

--- app/src/main/java/com/example/praknavigasi/view/SplashScreen.kt
+++ app/src/main/java/com/example/praknavigasi/view/SplashScreen.kt
@@ -1,5 +1,20 @@
 package com.example.praknavigasi.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.sp
+import com.example.praknavigasi.R
+import kotlinx.coroutines.delay
+
+
 @Composable
 fun SplashScreen(onTimeout: () -> Unit) {
     val context = LocalContext.current
@@ -8,16 +23,17 @@ fun SplashScreen(onTimeout: () -> Unit) {
         delay(2000)
         onTimeout()
     }
+
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(Color(context.getColor(R.color.colorAccent))),
         contentAlignment = Alignment.Center
-    )
-    Text(
-        text = stringResource(R.string.splash_title),
-        fontSize = 22.sp,
-        color = Color.White
-    )
-}
+    ) {
+        Text(
+            text = stringResource(R.string.splash_title),
+            fontSize = 22.sp,
+            color = Color.White
+        )
+    }
 }
