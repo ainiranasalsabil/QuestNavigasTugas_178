8981c308541a3faae8f47c5b3c9b1c6eb4cac389
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-11-07 16:04:20 +0700

    Membuat struktur awal Composable dengan kolom sederhana dan teks placeholder tanpa desain.

--- app/src/main/java/com/example/praknavigasi/view/MenuUtama.kt
+++ app/src/main/java/com/example/praknavigasi/view/MenuUtama.kt
@@ -1,2 +1,12 @@
 package com.example.praknavigasi.view
 
+@Composable
+fun MenuUtama(onMulaiClick: () -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Menu Utama")
+    }
+}
