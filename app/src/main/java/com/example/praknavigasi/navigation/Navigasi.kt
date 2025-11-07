61b38b3a12689bbea6fab1aa91cbeb655052596a
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-11-07 16:29:30 +0700

    Tambahkan enum Screen

--- app/src/main/java/com/example/praknavigasi/navigation/Navigasi.kt
+++ app/src/main/java/com/example/praknavigasi/navigation/Navigasi.kt
@@ -1,10 +1,20 @@
 package com.example.praknavigasi.navigation
 
+package com.example.navigasi.navigation
+
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 
+enum class Screen {
+    Splash,
+    Menu,
+    Form,
+    PopUp
+}
+
 @Composable
 fun Navigasi(navController: NavHostController = rememberNavController()) {
-    // Navigasi awal akan diatur kemudian
+    // Akan diisi dengan NavHost nanti
 }
+
