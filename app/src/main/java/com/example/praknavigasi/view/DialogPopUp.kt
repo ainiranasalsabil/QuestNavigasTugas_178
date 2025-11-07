ea4b558e8bec60b6824b2fd242dda074b644df05
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-11-07 16:23:05 +0700

    Tambah Column dan layout dasar

--- app/src/main/java/com/example/praknavigasi/view/DialogPopUp.kt
+++ app/src/main/java/com/example/praknavigasi/view/DialogPopUp.kt
@@ -1,9 +1,21 @@
 package com.example.praknavigasi.view
 
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun DialogPopUp(onBackClick: () -> Unit) {
-    Text(text = "Data berhasil dikirim!")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Data berhasil dikirim!")
+    }
 }
