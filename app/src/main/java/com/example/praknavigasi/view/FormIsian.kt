cf9e336bc562386181dd795fcd49fee8e15dd6d3
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-11-07 16:13:18 +0700

    Buat struktur dasar FormIsian

--- app/src/main/java/com/example/praknavigasi/view/FormIsian.kt
+++ app/src/main/java/com/example/praknavigasi/view/FormIsian.kt
@@ -1,2 +1,12 @@
 package com.example.praknavigasi.view
 
+@Composable
+fun FormIsian(onSubmitClick: () -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Form Isian")
+    }
+}
