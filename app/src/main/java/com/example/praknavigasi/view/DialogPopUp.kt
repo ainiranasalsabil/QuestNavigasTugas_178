947452d5a6c1d23370aba6f2e593af7517ac227d
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-11-07 16:24:44 +0700

    Tambah Scaffold dan TopAppBar

--- app/src/main/java/com/example/praknavigasi/view/DialogPopUp.kt
+++ app/src/main/java/com/example/praknavigasi/view/DialogPopUp.kt
@@ -7,21 +7,27 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DialogPopUp(onBackClick: () -> Unit) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(24.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(text = "Data berhasil dikirim!")
-
-        Spacer(modifier = Modifier.height(24.dp))
-
-        Button(onClick = onBackClick) {
-            Text(text = "Kembali ke Menu")
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text("Data Terkirim") })
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = "Data berhasil dikirim!")
+            Spacer(modifier = Modifier.height(24.dp))
+            Button(onClick = onBackClick) {
+                Text("Kembali ke Menu")
+            }
         }
     }
 }
