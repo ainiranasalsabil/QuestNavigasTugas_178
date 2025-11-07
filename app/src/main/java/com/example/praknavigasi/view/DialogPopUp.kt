2d8311b11059370b72a05ed8760b2613f8737941
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-11-07 16:21:19 +0700

    Tambah struktur dasar composable

--- app/src/main/java/com/example/praknavigasi/view/DialogPopUp.kt
+++ app/src/main/java/com/example/praknavigasi/view/DialogPopUp.kt
@@ -1,1 +1,9 @@
 package com.example.praknavigasi.view
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DialogPopUp(onBackClick: () -> Unit) {
+    Text(text = "Data berhasil dikirim!")
+}
