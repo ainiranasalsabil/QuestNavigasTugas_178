d1e678fb1251b9c66ee137b7bfa3b46d1310c784
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-11-07 16:27:05 +0700

    Tambahkan fontWeight dan perbaiki layout

--- app/src/main/java/com/example/praknavigasi/view/DialogPopUp.kt
+++ app/src/main/java/com/example/praknavigasi/view/DialogPopUp.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -33,6 +34,7 @@ fun DialogPopUp(onBackClick: () -> Unit) {
             Text(
                 text = "Data berhasil dikirim!",
                 fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
                 color = Color(0xFF1565C0)
             )
 
