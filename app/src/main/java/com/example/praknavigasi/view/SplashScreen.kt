870003f190c0b619c4d42d342add324ed1bad8e2
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-11-07 15:56:45 +0700

    Navigasi otomatis setelah delay 2 detik

--- app/src/main/java/com/example/praknavigasi/view/SplashScreen.kt
+++ app/src/main/java/com/example/praknavigasi/view/SplashScreen.kt
@@ -3,3 +3,8 @@ package com.example.praknavigasi.view
 @Composable
 fun SplashScreen(onTimeout: () -> Unit) {
     val context = LocalContext.current
+
+    LaunchedEffect(Unit) {
+        delay(2000)
+        onTimeout()
+    }
